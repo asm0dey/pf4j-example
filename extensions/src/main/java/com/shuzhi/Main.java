639bae21714990aafd55df89e54d566047052e93
Tigers <1343729568@qq.com>
2025-07-03 20:15:57 +0800

    fix(extensions&plugins): Fix plug-in location after packaging
    In the specification definition of PF4J, the directory of the plug-in must be a plugins directory.
    If you need to specify the directory to load, you need to pass "-Dpf4j.pluginsDir=xxxx" to VM options.

---
+++ extensions/src/main/java/com/shuzhi/Main.java
@@ -0,0 +1,13 @@
+package com.shuzhi;
+
+/// <h2> ${NAME} <h2/>
+///
+///
+/// @author Tigers Leung
+/// @version ${version}
+/// @since ${version}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
